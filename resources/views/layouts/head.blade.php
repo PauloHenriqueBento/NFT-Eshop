6f792135268355f063daed4b10d751580c584c3f
Eriveltoon <velton.sb@gmail.com>
2022-04-06 00:06:51 -0300

    Subindo p/ continuar na sala

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -6,11 +6,51 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+        rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+            crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+            crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="/css/home.css">
+    <title>@yield('title')</title>
 </head>
 <body>
-    <header>NavBar</header>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-primary bg-dark">
+            <div>
+                <a class="navbar-brand" href="#">Logo</a>
+            </div>
+            <form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="buscar">
+                <!--<button class="btn btn-outline-success" type="submit">Search</button>-->
+            </form>
+            <div class="d-flex justify-content-end">
+                <div class="dropdown d-flex align-items-center">
+                    <button class="btn btn-dark dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
+                    Explore
+                    </button>
+                    <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
+                        <li><a class="dropdown-item active" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <ul class="text-white navbar-nav m-3">
+                        <li class="nav-item px-2"><a href="#" class="text-decoration-none text-reset">Sobre</a></li>
+                        <li class="nav-item px-2"><a href="#" class="text-decoration-none text-reset">FAQ</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <main>
+        @yield('content')
+        <footer>
+            <p>NFT-Eshop &copy; 2022</p>
+        </footer>
+    </main>
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,132 @@
 @extends('layouts.head')
+
+@section('title', 'NFT - Eshop')
+
+@section('content')
+<section id="meuCarousel" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#meuCarousel" data-bs-slide-to="0" class="active"></button>
+        <button type="button" data-bs-target="#meuCarousel" data-bs-slide-to="1"></button>
+        <button type="button" data-bs-target="#meuCarousel" data-bs-slide-to="2"></button>
+
+    </div>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="https://cdn.cloudflare.steamstatic.com/steam/apps/1449850/header.jpg" class="d-block w-100">
+        </div>
+        <div class="carousel-item">
+            <img src="https://cdn.cloudflare.steamstatic.com/steam/apps/1449850/header.jpg" class="d-block w-100">
+        </div>
+        <div class="carousel-item">
+            <img src="https://cdn.cloudflare.steamstatic.com/steam/apps/1449850/header.jpg" class="d-block w-100">
+        </div>
+    </div>
+
+    <button class="carousel-control-prev" type="button" data-bs-target="#meuCarousel" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon"></span>
+        <span class="visually-hidden"> Voltar</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#meuCarousel" data-bs-slide="next">
+        <span class="carousel-control-next-icon"></span>
+        <span class="visually-hidden">Próximo</span>
+    </button>
+</section>
+
+<section class="container">
+    <div class="mt-5">
+        <h2 class="text-white">Tendências</h2>
+    </div>
+    <div class="row pt-2">
+        <div class="col-3">
+            <div class="divnft">
+                <img src="/images/nft.gif" class="nft">
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="divnft">
+                <img src="/images/nft.gif" class="nft">
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="divnft">
+                <img src="/images/nft.gif" class="nft">
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="divnft">
+                <img src="/images/nft.gif" class="nft">
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="container">
+    <div class="mt-5">
+        <h2 class="text-white">Top Criadores</h2>
+    </div>
+    <div class="row pt-2">
+        <div class="col-3">
+            <div class="divnft-2">
+                <img src="https://wallpapercave.com/wp/wp10537280.jpg" class="nft-criadores">
+                <div class="d-flex justify-content-center">
+                    <img src="https://public.nftstatic.com/static/nft/zipped/18b5012fbd8341d2a6a2d994a2e6509a_zipped.jpeg" class="icon">
+                </div>
+                <div class="d-flex justify-content-center pt-1">
+                    <span>NFT LION</span>
+                </div>
+                <div class="d-flex justify-content center ps-3 pt-1">
+                    <p>Voice Life F-NFT represents the tokenization of its intellectual property and
+                        groundbreaking wireless power.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="divnft-2">
+                <img src="https://wallpapercave.com/wp/wp10537280.jpg" class="nft-criadores">
+                <div class="d-flex justify-content-center">
+                    <img src="https://public.nftstatic.com/static/nft/zipped/18b5012fbd8341d2a6a2d994a2e6509a_zipped.jpeg" class="icon">
+                </div>
+                <div class="d-flex justify-content-center pt-1">
+                    <span>NFT LION</span>
+                </div>
+                <div class="d-flex justify-content center ps-3 pt-1">
+                    <p>Voice Life F-NFT represents the tokenization of its intellectual property and
+                        groundbreaking wireless power.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="divnft-2">
+                <img src="https://wallpapercave.com/wp/wp10537280.jpg" class="nft-criadores">
+                <div class="d-flex justify-content-center">
+                    <img src="https://public.nftstatic.com/static/nft/zipped/18b5012fbd8341d2a6a2d994a2e6509a_zipped.jpeg" class="icon">
+                </div>
+                <div class="d-flex justify-content-center pt-1">
+                    <span>NFT LION</span>
+                </div>
+                <div class="d-flex justify-content center ps-3 pt-1">
+                    <p>Voice Life F-NFT represents the tokenization of its intellectual property and
+                        groundbreaking wireless power.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="divnft-2">
+                <img src="https://wallpapercave.com/wp/wp10537280.jpg" class="nft-criadores">
+                <div class="d-flex justify-content-center">
+                    <img src="https://public.nftstatic.com/static/nft/zipped/18b5012fbd8341d2a6a2d994a2e6509a_zipped.jpeg" class="icon">
+                </div>
+                <div class="d-flex justify-content-center pt-1">
+                    <span>NFT LION</span>
+                </div>
+                <div class="d-flex justify-content center ps-3 pt-1">
+                    <p>Voice Life F-NFT represents the tokenization of its intellectual property and
+                        groundbreaking wireless power.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+@endsection
