32ed11ec2ca7bfcba72eefa636393be8d2ec9a9f
Eriveltoon <velton.sb@gmail.com>
2022-05-15 21:42:24 -0300

    Crud da TAG

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TagController;
+use App\Http\Controllers\Teste_Product;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/tag/create', [TagController::class, 'create'])->name('tag.create');
+Route::post('/tag/create', [TagController::class, 'store'])->name('tag.store');
+Route::get('/tag',[TagController::class, 'index'])->name('tag.index');
+
+Route::get('/tag/destroy/{tag}', [TagController::class, 'destroy'])->name('tag.destroy');
+Route::get('/tag/edit/{tag}', [TagController::class, 'edit'])->name('tag.edit');
+Route::put('/tag/edit/{tag}', [TagController::class, 'update'])->name('tag.update');
+Route::get('/tag/trash', [TagController::class, 'trash'])->name('tag.trash');
+Route::get('/tag/restore/{tag}', [TagController::class, 'restore'])->name('tag.restore');
